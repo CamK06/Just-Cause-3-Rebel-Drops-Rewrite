e213c0060f3ce68e3f54fbd34d93d35d59079e54
Starman <CamdenKerby@gmail.com>
2020-06-04 00:30:19 -0400

    Added project files

---
+++ Just Cause 3 Rebel Drops Rewrite/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GTA;
+
+namespace JustCauseRebelDrops
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Tick += OnTick;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
